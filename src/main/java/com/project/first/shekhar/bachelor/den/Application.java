5ce002e503271dd0c72e167206af6976d7729bf3
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-04 17:49:00 +0530

    made the homepage...first commit

--- src/main/java/com/project/first/shekhar/bachelor/den/Application.java
+++ src/main/java/com/project/first/shekhar/bachelor/den/Application.java
@@ -13,8 +13,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("This is the Home Page");
 	}
 }
