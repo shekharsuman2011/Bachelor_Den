6593f91b44e784ad7566c0622515a0bbe3cefb0a
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-09 14:36:10 +0530

    adding a user entity to see if table is being created or not

---
+++ src/main/java/com/project/first/shekhar/bachelor/den/entities/User.java
@@ -0,0 +1,25 @@
+package com.project.first.shekhar.bachelor.den.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int ID;
+    @Column(name="user_name",nullable=false)
+    private String name;
+    private String email;
+    private String password;
+
+
+}
