0065e3d02a308692cd5784aa53b67c1bbcf38d58
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-18 20:54:28 +0530

    implementing all the function in the UserServiceImpl class

--- src/main/java/com/project/first/shekhar/bachelor/den/services/impl/UserServiceImpl.java
+++ src/main/java/com/project/first/shekhar/bachelor/den/services/impl/UserServiceImpl.java
@@ -1,38 +1,55 @@
 package com.project.first.shekhar.bachelor.den.services.impl;
 
 import com.project.first.shekhar.bachelor.den.entities.User;
+import com.project.first.shekhar.bachelor.den.exceptions.ResourceNotFoundException;
 import com.project.first.shekhar.bachelor.den.payloads.UserDto;
 import com.project.first.shekhar.bachelor.den.repositories.UserRepo;
 import com.project.first.shekhar.bachelor.den.services.UserService;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService {
 
     private UserRepo userRepo;
     @Override
-    public UserDto createUser(UserDto user) {
-        return null;
+    public UserDto createUser(UserDto userDto) {
+        User user=this.dtoToUser(userDto);
+        User savedUser=this.userRepo.save(user);
+        return this.userToDto(savedUser);
     }
 
     @Override
-    public UserDto updateUser(UserDto user, Integer Id) {
-        return null;
+    public UserDto updateUser(UserDto userDto, Integer userId) {
+        User user=this.userRepo.findById(userId)
+                        .orElseThrow(() -> new ResourceNotFoundException("user","id",userId));
+        user.setName(userDto.getName());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
+        return this.userToDto(userRepo.save(user));
     }
 
     @Override
-    public UserDto getUserById(Integer Id) {
-        return null;
+    public UserDto getUserById(Integer userId) {
+        User user=this.userRepo.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("user","id",userId));
+        return userToDto(user);
     }
 
     @Override
     public List<UserDto> getAllUser() {
-        return null;
+        List<User> users=userRepo.findAll();
+        List<UserDto> userDtos= users.stream()
+                .map(user ->this.userToDto(user)).collect(Collectors.toList());
+        return userDtos;
     }
 
     @Override
-    public void deleteUser(Integer Id) {
-
+    public void deleteUser(Integer userId) {
+        User user=userRepo.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("user","id",userId));;
+        userRepo.delete(user);
     }
 
     User dtoToUser(UserDto userDto)
