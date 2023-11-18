d5735e9fd597c902b7a473d2e3c994ad131d8274
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-18 21:05:36 +0530

    adding autowire annotation

--- src/main/java/com/project/first/shekhar/bachelor/den/controllers/UserController.java
+++ src/main/java/com/project/first/shekhar/bachelor/den/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.project.first.shekhar.bachelor.den.controllers;
 
 import com.project.first.shekhar.bachelor.den.payloads.UserDto;
 import com.project.first.shekhar.bachelor.den.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
-
+    @Autowired
     private UserService userService;
     @GetMapping("/")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
