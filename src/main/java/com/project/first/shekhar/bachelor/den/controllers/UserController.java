d46f1121aa74fedabb05265cab0f31627ba7d3a6
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-18 21:18:54 +0530

    correcting the annotation from getmapping to postmapping

--- src/main/java/com/project/first/shekhar/bachelor/den/controllers/UserController.java
+++ src/main/java/com/project/first/shekhar/bachelor/den/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.project.first.shekhar.bachelor.den.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,7 +13,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    @GetMapping("/")
+    @PostMapping("/")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
     {
         UserDto createdUserDto=userService.createUser(userDto);
