59e76b60e9b6b879ca2645e6f66b029062439f14
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-18 21:05:08 +0530

    adding method to create a user

--- src/main/java/com/project/first/shekhar/bachelor/den/controllers/UserController.java
+++ src/main/java/com/project/first/shekhar/bachelor/den/controllers/UserController.java
@@ -1,5 +1,11 @@
 package com.project.first.shekhar.bachelor.den.controllers;
 
+import com.project.first.shekhar.bachelor.den.payloads.UserDto;
+import com.project.first.shekhar.bachelor.den.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
+    private UserService userService;
+    @GetMapping("/")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
+    {
+        UserDto createdUserDto=userService.createUser(userDto);
+        return new ResponseEntity<>(createdUserDto,HttpStatus.CREATED);
+    }
 
 }
