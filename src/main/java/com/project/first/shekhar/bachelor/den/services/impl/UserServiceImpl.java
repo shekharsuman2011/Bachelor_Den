999e1e2e05b1e501dcb2e10fef2ec3d8978cc9ef
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2023-11-18 20:04:52 +0530

    mapping of user to userdto and vice a versa

--- src/main/java/com/project/first/shekhar/bachelor/den/services/impl/UserServiceImpl.java
+++ src/main/java/com/project/first/shekhar/bachelor/den/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.project.first.shekhar.bachelor.den.services.impl;
 
+import com.project.first.shekhar.bachelor.den.entities.User;
 import com.project.first.shekhar.bachelor.den.payloads.UserDto;
 import com.project.first.shekhar.bachelor.den.repositories.UserRepo;
 import com.project.first.shekhar.bachelor.den.services.UserService;
@@ -32,5 +33,25 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteUser(Integer Id) {
 
+    }
+
+    User dtoToUser(UserDto userDto)
+    {
+        User user=new User();
+        user.setID(userDto.getID());
+        user.setName(userDto.getName());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
+        return user;
+    }
+
+    UserDto userToDto(User user)
+    {
+        UserDto userDto=new UserDto();
+        userDto.setID(user.getID());
+        userDto.setName(user.getName());
+        userDto.setPassword(user.getPassword());
+        userDto.setEmail(user.getEmail());
+        return userDto;
     }
 }
